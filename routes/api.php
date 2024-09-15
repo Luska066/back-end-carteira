a56184e5bc1ff847a6f28d62889e311d1bf8b729
Luska066 <lucas.santsena@gmail.com>
2024-09-15 16:50:05 -0300

    mend

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,16 @@ Route::middleware('auth:api')->prefix('/v1')->group( function () {
 Route::prefix('public')->group( function () {
     Route::post('/student/web-socket', function (Request $request) {
         try {
+            \Illuminate\Support\Facades\Log::info("request websocket",[
+                "request" => $request
+            ]);
             \App\Events\PaymentResponse::dispatch($request->user());
         } catch (Exception $e) {
+            \Illuminate\Support\Facades\Log::info("Error ao executar websocket",[
+                'error' => $e->getMessage(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile()
+            ]);
             dd($e->getMessage());
         }
         \Illuminate\Support\Facades\Log::info("SOCKET ENVIADO");
